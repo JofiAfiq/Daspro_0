442f6f4ea3a2185ad4c44d30a1bcb137343cb689
JofiAfiq <jofitri.afiq@gmail.com>
2025-11-05 15:02:57 +0700

    update folder LTIK collab

---
+++ 11-05-25-LTIK/github test.py
@@ -0,0 +1,12 @@
+#jofi tri fathan afiq
+#2510457
+
+def fibonacci(n):
+    deret = [0, 1]
+    for i in range(2, n):
+        next = deret[i-1] + deret [i-2]
+        deret.append(next)
+
+    return deret[:n]
+
+print(fibonacci(100))
